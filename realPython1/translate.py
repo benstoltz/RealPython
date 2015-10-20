1b6e5f154e7d78a7df5a4326e630b0daa845a897
Ben Stoltz <bstoltz@esri.com>
2015-10-20 16:26:34 -0400

    Initial Commit
    Initial Commit

---
+++ realPython1/translate.py
@@ -0,0 +1,6 @@
+def translate(words):
+    return words.replace("a", "4")\
+        .replace("b", "8").replace("e", "3")\
+        .replace("l", "1").replace("o", "0")\
+        .replace("s", "5").replace("t", "7")
+
